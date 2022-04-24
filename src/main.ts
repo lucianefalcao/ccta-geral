9a815a81c4910d03babc6f9881953b953290a0cb
Luciane Falcão <lucianefalcaoff@gmail.com>
2022-04-24 13:39:20 -0300

    remove configuração anterior

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { useRegisterSW } from 'virtual:pwa-register/vue'
-import './index.css'
-
-const updateSW = useRegisterSW({})
-createApp(App).mount('#app')
