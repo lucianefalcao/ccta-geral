fbe3c2e144dcc470527c08d8a6d477f8179f1b06
Luciane Falcão <lucianefalcaoff@gmail.com>
2022-05-02 04:54:07 -0300

    define estado de carregando

---
+++ src/components/ErroBanner.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  mensagem: string;
+}>();
+</script>
+
+<template>
+  <q-banner class="bg-grey-1" rounded>
+    <template v-slot:avatar>
+      <q-icon name="o_error_outline" color="primary" />
+    </template>
+    {{ mensagem }}
+  </q-banner>
+</template>
--- src/components/home/ListaNoticias.vue
+++ src/components/home/ListaNoticias.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import CardNoticia from './CardNoticia.vue';
+import ErroBanner from '../ErroBanner.vue';
 import { onMounted, ref } from '@vue/runtime-core';
 import Noticia from '../../models/domain/Noticia';
 import { useNoticiaStore } from '../../stores/noticia';
@@ -9,18 +10,39 @@ type NoticiaExibicao = Noticia & { rota: string };
 const noticiaStore = useNoticiaStore();
 
 const noticias = ref<NoticiaExibicao[]>([]);
+const carregando = ref<boolean>(false);
+const mensagemErro = ref<string>('');
 
 onMounted(async () => {
-  const dadosNoticias = await noticiaStore.getNoticias(3);
+  try {
+    mensagemErro.value = '';
+    carregando.value = true;
 
-  dadosNoticias.forEach((noticia) => {
-    noticias.value.push({ ...noticia, rota: '#' });
-  });
+    const dadosNoticias = await noticiaStore.getNoticias(3);
+    dadosNoticias.forEach((noticia) => {
+      noticias.value.push({ ...noticia, rota: '#' });
+    });
+  } catch (e: any) {
+    mensagemErro.value =
+      'Ocorreu um erro ao buscar as notícias, por favor atualize a página.';
+  } finally {
+    carregando.value = false;
+  }
 });
 </script>
 
 <template>
+  <ErroBanner v-if="!carregando && mensagemErro" :mensagem="mensagemErro" />
+
+  <q-circular-progress
+    v-if="carregando"
+    indeterminate
+    size="sm"
+    color="primary"
+    class="q-ma-md"
+  />
   <CardNoticia
+    v-else
     v-for="noticia in noticias"
     :key="noticia.titulo"
     :titulo="noticia.titulo"
