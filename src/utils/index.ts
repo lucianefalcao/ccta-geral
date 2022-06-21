b50aad87d46464db37f4c728cedf9e4bf903a362
Luciane Falcão <lucianefalcaoff@gmail.com>
2022-06-21 06:16:39 -0300

    adiciona método para retornar uma data legível

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,13 @@
 const lineClamp = (texto: string) => {
   if (texto.length > 145) {
-    return texto.slice(0, 145) + '&hellip;'
+    return texto.slice(0, 145) + '&hellip;';
   }
-  
-  return texto
-}
 
-export { lineClamp }
+  return texto;
+};
+
+const dataLegivel = (data: Date): string | null => {
+  return data?.toLocaleDateString('pt-BR') ?? null;
+};
+
+export { lineClamp, dataLegivel };
