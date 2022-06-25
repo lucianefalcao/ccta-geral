4bfc32110cebca3ab4dec37d9abc17d38289f608
Luciane Falcão <lucianefalcaoff@gmail.com>
2022-06-25 05:02:19 -0300

    busca os dados de eventos do banco

--- src/components/home/CardEvento.vue
+++ src/components/home/CardEvento.vue
@@ -1,23 +1,40 @@
 <script setup lang="ts">
-import { lineClamp } from '../../utils'
+import { computed } from '@vue/runtime-core';
+import Evento from 'src/models/domain/eventos/evento';
+import { useRouter } from 'vue-router';
+import { lineClamp, dataLegivel } from '../../utils';
 
-defineProps<{
-  titulo: string,
-  data: string,
-  hora: string,
-  descricao: string,
-  rota: string
-}>()
+const props = defineProps<{
+  evento: Evento;
+}>();
 
+const Router = useRouter();
+
+const data = computed<string>(() => {
+  return dataLegivel(props.evento.getData());
+});
+
+const hora = computed<string>(() => {
+  return props.evento
+    .getData()
+    ?.toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' });
+});
+
+const titulo = computed<string>(() => {
+  return props.evento.getTitulo();
+});
+
+const descricao = computed<string>(() => {
+  return props.evento.getDescricao();
+});
+
+const rota = computed<string>(() => {
+  return `/eventos/${props.evento.getId()}`;
+});
 </script>
 
 <template>
-  <q-card
-    class="col-md-3"
-    flat
-    bordered
-    style="max-width: 300px"
-  >
+  <q-card class="col-md-3" flat bordered style="max-width: 300px">
     <q-card-section class="q-pt-xs">
       <div class="text-overline">{{ data }}</div>
       <div class="text-h5 q-mt-sm q-mb-xs">{{ titulo }}</div>
@@ -27,12 +44,10 @@ defineProps<{
     <q-separator />
 
     <q-card-actions>
-      <q-btn disable flat round icon="o_insert_invitation" >
+      <q-btn disable flat round icon="o_insert_invitation">
         {{ hora }}
       </q-btn>
-      <q-btn flat color="primary" :to="rota">
-        Detalhes
-      </q-btn>
+      <q-btn flat color="primary" :to="rota"> Detalhes </q-btn>
     </q-card-actions>
   </q-card>
 </template>
--- src/components/home/ListaEventos.vue
+++ src/components/home/ListaEventos.vue
@@ -1,22 +1,45 @@
 <script setup lang="ts">
+import { ref } from '@vue/reactivity';
+import { onMounted } from '@vue/runtime-core';
+import Evento from 'src/models/domain/eventos/evento';
+import { useEventoStore } from 'src/stores/evento';
 import CardEvento from './CardEvento.vue';
+import ErroBanner from '../ErroBanner.vue';
 
-const eventos = [
-  { titulo: 'Título 1', descricao: 'descrição 1', data: '21/04/2022', hora: '19:00', rota: '#' },
-  { titulo: 'Título 2', descricao: 'descrição 2', data: '21/04/2022', hora: '19:00', rota: '#' },
-  { titulo: 'Título 3', descricao: 'descrição 3', data: '21/04/2022', hora: '19:00', rota: '#' },
-]
+const eventoStore = useEventoStore();
+const eventos = ref<Evento[]>([]);
+const carregando = ref<boolean>(false);
+const mensagemErro = ref<string>('');
 
+onMounted(async () => {
+  try {
+    mensagemErro.value = '';
+    carregando.value = true;
+    eventos.value = await eventoStore.getEventos();
+  } catch (e: any) {
+    mensagemErro.value =
+      'Ocorreu um erro ao buscar as notícias, por favor atualize a página.';
+  } finally {
+    carregando.value = false;
+  }
+});
 </script>
 
 <template>
+  <ErroBanner v-if="!carregando && mensagemErro" :mensagem="mensagemErro" />
+  <q-circular-progress
+    v-if="carregando"
+    indeterminate
+    size="sm"
+    color="primary"
+    class="q-ma-md"
+  />
+  <div v-if="!carregando && eventos.length === 0">
+    <p>Nenhuma notícia cadastrada</p>
+  </div>
   <CardEvento
     v-for="evento in eventos"
-    :key="evento.titulo"
-    :titulo="evento.titulo"
-    :descricao="evento.descricao"
-    :rota="evento.rota"
-    :data="evento.data"
-    :hora="evento.hora"
+    :key="evento.getId()"
+    :evento="evento"
   />
 </template>
