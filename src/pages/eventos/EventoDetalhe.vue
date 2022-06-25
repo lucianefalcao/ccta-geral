294b5389eef3706c9a7383e0a0b9a94b80d51fbb
Luciane Falcão <lucianefalcaoff@gmail.com>
2022-06-25 11:24:56 -0300

    cria telas de detalhes de evento

---
+++ src/pages/eventos/EventoDetalhe.vue
@@ -0,0 +1,151 @@
+<script setup lang="ts">
+import { useQuasar } from 'quasar';
+import { computed, onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { useUsuarioStore } from '../../stores/usuario';
+import ShareButton from 'src/components/ShareButton.vue';
+import { dataLegivel, adicionarEventoAgenda } from 'src/utils';
+import Usuario from 'src/models/domain/usuarios/usuario';
+import Evento from 'src/models/domain/eventos/evento';
+import { useEventoStore } from 'src/stores/evento';
+
+const eventoStore = useEventoStore();
+const usuarioStore = useUsuarioStore();
+const Route = useRoute();
+const $q = useQuasar();
+
+const evento = ref<Evento>();
+const usuarioCriador = ref<Usuario>();
+const usuarioEditor = ref<Usuario>();
+const carregando = ref<boolean>(false);
+const textoCompartilhamento = ref('');
+
+const editadoEm = computed<string | null>(() => {
+  return dataLegivel(evento.value.getEditadoEm());
+});
+
+const criadoEm = computed<string | null>(() => {
+  return dataLegivel(evento.value.getCriadoEm());
+});
+
+const data = computed<string | null>(() => {
+  return dataLegivel(evento.value.getData());
+});
+
+const hora = computed<string | null>(() => {
+  return evento.value
+    .getData()
+    ?.toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' });
+});
+
+const descricao = computed<string>(() => {
+  return evento.value.getDescricao();
+});
+
+const titulo = computed<string>(() => {
+  return evento.value.getTitulo();
+});
+
+const nomeUsuarioCriador = computed<string>(() => {
+  return usuarioCriador.value?.getNome() ?? 'Usuário não identificado';
+});
+
+const nomeUsuarioEditor = computed<string>(() => {
+  return usuarioEditor.value?.getNome() ?? 'Usuário não identificado';
+});
+
+onMounted(async () => {
+  try {
+    carregando.value = true;
+
+    if (!eventoStore.eventoSelecionado) {
+      const uid = Route.params.uid as string;
+      await eventoStore.getEvento(uid);
+    }
+
+    evento.value = eventoStore.eventoSelecionado as Evento;
+
+    usuarioCriador.value = await usuarioStore.getUsuario(
+      evento.value.getCriadoPor()
+    );
+
+    if (evento.value.getEditadoEm()) {
+      usuarioEditor.value = await usuarioStore.getUsuario(
+        evento.value.getEditadoPor()
+      );
+    }
+
+    textoCompartilhamento.value = `${evento.value.getTitulo()} ${
+      window.location.href
+    }`;
+  } catch (e) {
+    $q.notify({
+      color: 'negative',
+      message:
+        'Ocorreu um problema ao buscar a notícia. Por favor, atualize a página.',
+      icon: 'o_report_problem',
+      timeout: 5000,
+      position: 'top',
+    });
+  } finally {
+    carregando.value = false;
+  }
+});
+</script>
+
+<template>
+  <div class="row flex-center">
+    <q-spinner-hourglass
+      v-if="carregando || !evento?.getId()"
+      color="primary"
+      size="2em"
+    />
+    <div v-else>
+      <q-breadcrumbs class="q-mx-md-xl q-my-lg">
+        <q-breadcrumbs-el label="Início" icon="o_home" to="/" />
+        <q-breadcrumbs-el label="Eventos" icon="o_campaign" to="/eventos" />
+      </q-breadcrumbs>
+      <q-card
+        class="q-pa-md q-pa-md-xl q-mx-md-xl q-my-md q-mt-lg"
+        style="max-width: 950px"
+      >
+        <q-card-actions align="right">
+          <ShareButton :texto="textoCompartilhamento" />
+        </q-card-actions>
+
+        <q-card-section>
+          <p class="text-overline">{{ data }} - {{ hora }}</p>
+          <p class="text-h6 lt-md">{{ titulo }}</p>
+          <p class="text-h4 gt-sm">{{ titulo }}</p>
+          <p class="text-body2">
+            <span>
+              Escrito por {{ nomeUsuarioCriador }} &bull; escrito em
+              {{ criadoEm }}
+            </span>
+            <br />
+            <span v-if="editadoEm">
+              Editado por {{ nomeUsuarioEditor }} &bull; editado em
+              {{ editadoEm }}
+            </span>
+          </p>
+        </q-card-section>
+
+        <q-card-section>
+          <div v-html="descricao"></div>
+        </q-card-section>
+
+        <q-separator></q-separator>
+
+        <q-card-actions align="center">
+          <q-btn
+            :href="adicionarEventoAgenda(evento)"
+            target="_blank"
+            outline
+            color="primary"
+            label="Adicionar ao Google Agenda"
+          ></q-btn>
+        </q-card-actions>
+      </q-card>
+    </div>
+  </div>
+</template>
