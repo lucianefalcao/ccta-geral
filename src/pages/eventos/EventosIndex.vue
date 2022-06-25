5249de6787d6f1c496f638671ea88ed7a223b1da
Luciane Falcão <lucianefalcaoff@gmail.com>
2022-06-25 10:49:10 -0300

    cria página de eventos

---
+++ src/pages/eventos/EventosIndex.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { onMounted, ref } from '@vue/runtime-core';
+import Evento from 'src/models/domain/eventos/evento';
+import { useEventoStore } from 'src/stores/evento';
+import CardEvento from '../../components/home/CardEvento.vue';
+import ErroBanner from '../../components/ErroBanner.vue';
+import { lineClamp, dataLegivel } from '../../utils';
+import { useQuasar } from 'quasar';
+
+const eventoStore = useEventoStore();
+const $q = useQuasar();
+
+const eventos = ref<Evento[]>();
+const carregando = ref<boolean>(false);
+const mensagemErro = ref<string>('');
+
+onMounted(async () => {
+  try {
+    mensagemErro.value = '';
+    carregando.value = true;
+    eventos.value = await eventoStore.getEventos();
+  } catch (e: any) {
+    mensagemErro.value =
+      'Ocorreu um erro ao buscar as notícias, por favor atualize a página.';
+  } finally {
+    carregando.value = false;
+  }
+});
+</script>
+
+<template>
+  <div class="row flex-center">
+    <ErroBanner v-if="!carregando && mensagemErro" :mensagem="mensagemErro" />
+    <div v-else>
+      <q-breadcrumbs v-if="!carregando" class="q-mt-lg">
+        <q-breadcrumbs-el label="Início" icon="o_home" to="/" />
+      </q-breadcrumbs>
+      <p v-if="$q.screen.gt.sm" class="text-h4 q-mt-lg">
+        Eventos do mês de Junho
+      </p>
+      <p v-else class="text-h6 q-mt-lg">Eventos do mês de Junho</p>
+      <q-list class="column q-mb-sm q-mt-lg">
+        <q-item v-for="evento in eventos" :key="evento.getId()">
+          <CardEvento :evento="evento"></CardEvento>
+        </q-item>
+      </q-list>
+    </div>
+  </div>
+</template>
