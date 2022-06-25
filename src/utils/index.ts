0ce5e3fd3e3de4608c749bbd7cb50a7b088ca51b
Luciane Falcão <lucianefalcaoff@gmail.com>
2022-06-25 11:24:22 -0300

    adicionar o método no utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,6 @@
+import dayjs from 'dayjs';
+import Evento from 'src/models/domain/eventos/evento';
+
 const lineClamp = (texto: string) => {
   if (texto.length > 145) {
     return texto.slice(0, 145) + '&hellip;';
@@ -10,4 +13,18 @@ const dataLegivel = (data: Date | null): string | null => {
   return data?.toLocaleDateString('pt-BR') ?? null;
 };
 
-export { lineClamp, dataLegivel };
+const adicionarEventoAgenda = (evento: Evento) => {
+  const dataEvento = dayjs(evento.getData());
+  const inicio = dataEvento.toISOString().replace(/-|:|\./g, '');
+
+  const fim = dataEvento
+    .add(1, 'h')
+    .toISOString()
+    .replace(/-|:|\./g, '');
+
+  const local = 'Centro+De+Comunicação,+Turismo+e+Artes+-+CCTA/UFPB';
+
+  return `https://www.google.com/calendar/render?action=TEMPLATE&text=${evento.getTitulo()}&dates=${inicio}/${fim}&details=${evento.getDescricao()}&location=${local}&sf=true&output=xml`;
+};
+
+export { lineClamp, dataLegivel, adicionarEventoAgenda };
